439baf4edafc60d673bbaed8cc856c17e5ea473e
Denis Lindner <lindnerdenis19@gmail.com>
2025-08-22 13:04:48 -0300

    v1.1.0 Adição de Tratamento de Erros

---
+++ src/entradas/Entradas.java
@@ -0,0 +1,33 @@
+package entradas;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Entradas {
+    static Scanner SC = new Scanner(System.in);
+    public static int entradaInteiro(){
+        int numero;
+        while (true){
+            try {
+                numero = SC.nextInt();
+                break;
+            } catch (InputMismatchException e){
+                System.out.println("Entrada Inválida! Tente Novamente:");
+                SC.nextLine();
+            }
+        }
+        return numero;
+    }
+
+    public static String entradaString(){
+        String string;
+        do {
+            SC.nextLine();
+            string = SC.nextLine().toUpperCase();
+            if (string.isEmpty()){
+                System.out.println("Entrada Inválida! Tente Novamente:");
+            }
+        } while (string.isEmpty());
+        return string;
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -6,19 +6,18 @@ import model.ArtefatoDeCombate;
 import model.ArtefatoDeCura;
 import model.ArtefatoMagico;
 import model.Mago;
+import entradas.*;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static Scanner SC = new Scanner(System.in);
     public static void main(String[] args) throws  SQLException{
         while (true){
             System.out.println("=== Menu Principal ===\n1 - Cadastrar mago\n2 - Cadastrar artefato\n3 – Emprestar artefato para mago\n4 – Listar artefatos emprestados de um mago\n5 – Devolver artefato\n6 - Listar todos os artefatos disponíveis\n0 - Sair:\nEscolher:");
-            int opcao = SC.nextInt();
+            int opcao = Entradas.entradaInteiro();
             switch (opcao){
                 case 1 -> cadastrarMago();
                 case 2 -> cadastrarArtefato();
@@ -28,7 +27,6 @@ public class Main {
                 case 6 -> listarTodosArtefatosDisponiveis();
                 case 0 -> {
                     System.out.println("Saindo do Sistema...");
-                    SC.close();
                     System.exit(0);
                 }
                 default -> System.out.println("Opção Inválida!");
@@ -38,19 +36,19 @@ public class Main {
 
     public static void cadastrarMago() throws SQLException{
         System.out.println("Insira o ID do Mago:");
-        int idMago = SC.nextInt();
+        int idMago = Entradas.entradaInteiro();
         if (MagoDAO.existeCodigoMago(idMago)){
             System.out.println("Código já cadastrado!");
             return;
         }
 
         System.out.println("Insira o Nome do Mago:");
-        String nome = SC.next().trim().toUpperCase();
+        String nome = Entradas.entradaString();
 
         int nivel;
         do {
             System.out.println("Insira o Nível do Mago:");
-            nivel = SC.nextInt();
+            nivel = Entradas.entradaInteiro();
         } while (nivel < 1 || nivel > 20);
 
         MagoDAO.postMago(new Mago(idMago, nome, nivel));
@@ -58,26 +56,23 @@ public class Main {
     }
 
     public static void cadastrarArtefato() throws SQLException {
-        SC.nextLine();
         System.out.println("Insira o Nome do Artefato:");
-        String nome = SC.nextLine().toUpperCase();
+        String nome = Entradas.entradaString();
 
         int nivelMagia;
         do {
             System.out.println("Insira o Nível do Artefato:");
-            nivelMagia = SC.nextInt();
-            SC.nextLine();
+            nivelMagia = Entradas.entradaInteiro();
         } while (nivelMagia < 1 || nivelMagia > 10);
 
         System.out.println("Insira a Descrição do Artefato:");
-        String descricao = SC.nextLine().toUpperCase();
+        String descricao = Entradas.entradaString();
 
         ArtefatoMagico artefatoMagico = null;
         boolean loop = true;
         while (loop){
             System.out.println("Qual o Tipo do Artefato?\n1 - Cura\n2 - Ataque\nEscolha:");
-            int opcao = SC.nextInt();
-            SC.nextLine();
+            int opcao = Entradas.entradaInteiro();
             switch (opcao){
                 case 1 -> {
                     artefatoMagico = new ArtefatoDeCura(nome, nivelMagia, descricao);
@@ -109,7 +104,7 @@ public class Main {
         listarTodosArtefatosDisponiveis();
 
         System.out.println("Insira o código do Artefato:");
-        String codigo = SC.next().toUpperCase().trim();
+        String codigo = Entradas.entradaString();
 
         ArtefatoMagico artefatoMagico = ArtefatoDAO.buscarArtfato(codigo);
         if (artefatoMagico == null){
@@ -129,7 +124,7 @@ public class Main {
         }
 
         System.out.println("Insira o id do Mago:");
-        int id = SC.nextInt();
+        int id = Entradas.entradaInteiro();
 
         Mago mago = MagoDAO.buscarMago(id);
         if (mago == null){
@@ -158,19 +153,21 @@ public class Main {
         }
 
         System.out.println("Insira o id do Mago:");
-        int id = SC.nextInt();
+        int id = Entradas.entradaInteiro();
 
         Mago mago = MagoDAO.buscarMago(id);
         if (mago == null){
             System.out.println("Mago não encontrado!");
             return;
         }
+
         System.out.println("Mago "+mago.getNome()+" Escolhido!");
         ArrayList<ArtefatoMagico> artefatos = mago.getArtefatos();
         if (artefatos.isEmpty()){
             System.out.println("Nenhum Artefato Emprestado!");
             return;
         }
+
         System.out.println("Artefatos Emprestados:");
         for (ArtefatoMagico artefatoMagico : artefatos){
             System.out.println(artefatoMagico.toString()+"\n");
@@ -190,7 +187,7 @@ public class Main {
         }
 
         System.out.println("Insira o id do Mago:");
-        int id = SC.nextInt();
+        int id = Entradas.entradaInteiro();
 
         Mago mago = MagoDAO.buscarMago(id);
         if (mago == null){
@@ -209,7 +206,7 @@ public class Main {
         }
 
         System.out.println("Insira o Código do artefato que deseja Devolver:");
-        String codigo = SC.next().toUpperCase().trim();
+        String codigo = Entradas.entradaString();
 
         ArtefatoMagico artefatoMagico = ArtefatoDAO.buscarArtfato(codigo);
         if (artefatoMagico == null){
